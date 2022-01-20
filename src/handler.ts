51c509cf6e081fb9c7515d33cd0eb026c6043438
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-01-20 17:11:36 +0100

    collect urls and fetch data

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,36 @@
+// capturing groups: 1: href=" 3: https://test.url 4: "
+const hrefRegexp = /(href=(['"]))((?:https?|ipfs):\/\/.+?)(\2)/gim
+
+// capturing groups: 1: url(" 3: https://test.url 4: ")
+const cssUrlRegexp = /(url\(\s*(['"]?))((?:https?|ipfs):\/\/.+?)(\2\s*\))/gim
+
+type UrlOccurrences = {
+  [url: string]: number[] // string indices of occurrences
+}
+export function collectUrls(svg: string): UrlOccurrences {
+  const matches = [...svg.matchAll(hrefRegexp), ...svg.matchAll(cssUrlRegexp)]
+  return matches.reduce((result, match) => {
+    if (match.index === undefined) return result
+    const prefix = match[1]
+    const url = match[3]
+    if (!result[url]) result[url] = []
+    result[url].push(match.index + prefix.length)
+    return result
+  }, {} as UrlOccurrences)
+}
+
+export async function fetchData(url: string): Promise<string> {
+  const response = await fetch(url)
+  if (!response.ok || !response.body) {
+    throw new Error('could not fetch')
+  }
+
+  const contentType = response.headers.get('Content-Type')
+  const bytes = new Uint8Array(await response.arrayBuffer())
+  const binary = String.fromCharCode(...bytes)
+  return `data:${contentType};base64,${btoa(binary)}`
+}
+
 export async function handleRequest(request: Request): Promise<Response> {
   if (request.method !== 'GET') {
     return new Response(null, {
@@ -11,11 +44,14 @@ export async function handleRequest(request: Request): Promise<Response> {
   if (!pathname.startsWith(prefix)) {
     return new Response(null, {
       status: 400,
-      statusText: 'path must be a data: URI',
+      statusText: 'Path must be a data: URI',
     })
   }
 
   const svg = atob(pathname.substring(prefix.length))
+  const urls = collectUrls(svg)
+
+  const dataUris = await Promise.allSettled(Object.keys(urls).map(fetchData))
 
   return new Response(`request path: ${svg}`)
 }
--- test/handler.test.ts
+++ test/handler.test.ts
@@ -1,4 +1,4 @@
-import { handleRequest } from '../src/handler'
+import { handleRequest, collectUrls, fetchData } from '../src/handler'
 import makeServiceWorkerEnv from 'service-worker-mock'
 
 declare const global: any
@@ -9,10 +9,71 @@ describe('handle', () => {
     jest.resetModules()
   })
 
-  test('handle GET', async () => {
-    const result = await handleRequest(new Request('/', { method: 'GET' }))
+  it('handle GET', async () => {
+    const result = await handleRequest(
+      new Request(
+        '/data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxpbWFnZSBocmVmPSJodHRwczovL2dhdGV3YXkucGluYXRhLmNsb3VkL2lwZnMvUW1UUXJQR0RmMnhpZ0FLMnB0RGhka3ZTRjJFZlJNWHBhRkdKS0JOUktZUkJIdiIgaGVpZ2h0PSIyMDAiIHdpZHRoPSIyMDAiLz4KPC9zdmc+',
+        { method: 'GET' },
+      ),
+    )
     expect(result.status).toEqual(200)
     const text = await result.text()
     expect(text).toEqual('request method: GET')
+  })
+})
+
+describe('collectUrls', () => {
+  beforeEach(() => {
+    Object.assign(global, makeServiceWorkerEnv())
+    jest.resetModules()
+  })
+
+  it('handles image href', () => {
+    expect(
+      collectUrls(`<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+        <image href="https://gateway.pinata.cloud/ipfs/QmTQrPGDf2xigAK2ptDhdkvSF2EfRMXpaFGJKBNRKYRBHv" height="200" width="200"/>
+        <image href='ipfs://QmTQrPGDf2xigAK2ptDhdkvSF2EfRMXpaFGJKBNRKYRBHv' height="200" width="200"/>
+      </svg>`),
+    ).toMatchInlineSnapshot(`
+      Object {
+        "https://gateway.pinata.cloud/ipfs/QmTQrPGDf2xigAK2ptDhdkvSF2EfRMXpaFGJKBNRKYRBHv": Array [
+          87,
+        ],
+        "ipfs://QmTQrPGDf2xigAK2ptDhdkvSF2EfRMXpaFGJKBNRKYRBHv": Array [
+          217,
+        ],
+      }
+    `)
+  })
+
+  it('handles css urls', () => {
+    expect(
+      collectUrls(`<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg"><defs>
+        <style type="text/css">@import url(http://fonts.googleapis.com/css?family=Indie+Flower);</style>
+      </defs></svg>`),
+    ).toMatchInlineSnapshot(`
+      Object {
+        "http://fonts.googleapis.com/css?family=Indie+Flower": Array [
+          115,
+        ],
+      }
+    `)
+  })
+})
+
+describe('fetchData', () => {
+  beforeEach(() => {
+    Object.assign(global, makeServiceWorkerEnv())
+    jest.resetModules()
+  })
+
+  it('returns data URI', async () => {
+    expect(
+      await fetchData(
+        'https://gateway.pinata.cloud/ipfs/QmaVXNTjcbezSZj8BCxURZdB568e3ppQCnFDu7nYmfTL9y',
+      ),
+    ).toMatchInlineSnapshot(
+      `"data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxpbWFnZSBocmVmPSJodHRwczovL2dhdGV3YXkucGluYXRhLmNsb3VkL2lwZnMvUW1UUXJQR0RmMnhpZ0FLMnB0RGhka3ZTRjJFZlJNWHBhRkdKS0JOUktZUkJIdiIgaGVpZ2h0PSIyMDAiIHdpZHRoPSIyMDAiLz4KICAgIDxpbWFnZSBocmVmPSJodHRwczovL2dhdGV3YXkucGluYXRhLmNsb3VkL2lwZnMvUW1kU0RxaDJnMTNHVVcybnZQa3dBQ0g1cTE2VlpxVlJMRjRSU1B6REw0REF0NiIgaGVpZ2h0PSIyMDAiIHdpZHRoPSIyMDAiLz4KPC9zdmc+"`,
+    )
   })
 })
