ad7f7fae6c5ab0eb05c999f48a8b83a33d4488a8
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-01-20 13:50:03 +0100

    respond svg string

--- src/handler.ts
+++ src/handler.ts
@@ -2,11 +2,20 @@ export async function handleRequest(request: Request): Promise<Response> {
   if (request.method !== 'GET') {
     return new Response(null, {
       status: 405,
-      statusText: 'Method Not Allowed',
+      statusText: 'Only GET allowed',
     })
   }
 
-  const url = new URL(request.url)
+  const { pathname } = new URL(request.url)
+  const prefix = '/data:image/svg+xml;base64,'
+  if (!pathname.startsWith(prefix)) {
+    return new Response(null, {
+      status: 400,
+      statusText: 'path must be a data: URI',
+    })
+  }
 
-  return new Response(`request path: ${url.pathname}`)
+  const svg = atob(pathname.substring(prefix.length))
+
+  return new Response(`request path: ${svg}`)
 }
