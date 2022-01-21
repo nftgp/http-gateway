85949015a299f2de22464916fe420714417544ed
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-01-21 13:46:43 +0100

    mock ReadableStream API

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
-require('isomorphic-fetch')
+const isoFetch = require('isomorphic-fetch')
 
 if (typeof btoa === 'undefined') {
   global.btoa = function (str) {
@@ -10,4 +10,32 @@ if (typeof atob === 'undefined') {
   global.atob = function (b64Encoded) {
     return new Buffer(b64Encoded, 'base64').toString('binary')
   }
+}
+
+global.fetch = async (...args) => {
+  const response = await isoFetch(...args)
+  const body = response.body
+  body.getReader = () => {
+    let done = false
+
+    return {
+      read: () => {
+        if (done) {
+          return Promise.resolve({ done })
+        }
+
+        return new Promise((resolve) => {
+          let resolved = false
+          body.once('readable', () => {
+            resolve({ value: body.read(), done: false })
+          })
+          body.once('end', () => {
+            done = true
+            if (!resolved) resolve({ done })
+          })
+        })
+      },
+    }
+  }
+  return response
 }
