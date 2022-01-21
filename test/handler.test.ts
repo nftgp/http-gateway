8e74ccc8d900c3f2f72d5992c46fc7dddf0e8e15
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-01-21 15:34:55 +0100

    fix tests

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -1,8 +1,6 @@
 import { handleRequest, collectUrls, fetchData } from '../src/handler'
 import makeServiceWorkerEnv from 'service-worker-mock'
 
-declare const global: any
-
 describe('handle', () => {
   beforeEach(() => {
     Object.assign(global, makeServiceWorkerEnv())
@@ -12,13 +10,17 @@ describe('handle', () => {
   it('handle GET', async () => {
     const result = await handleRequest(
       new Request(
-        '/data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxpbWFnZSBocmVmPSJodHRwczovL2dhdGV3YXkucGluYXRhLmNsb3VkL2lwZnMvUW1UUXJQR0RmMnhpZ0FLMnB0RGhka3ZTRjJFZlJNWHBhRkdKS0JOUktZUkJIdiIgaGVpZ2h0PSIyMDAiIHdpZHRoPSIyMDAiLz4KPC9zdmc+',
+        '/data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMDAiIGhlaWdodD0iMjAwIj4KICAgIDxpbWFnZSBocmVmPSJodHRwczovL2dhdGV3YXkucGluYXRhLmNsb3VkL2lwZnMvUW1XNXptUjJjOXhqeU03OTVWUkFjRjViV0g5NGNtemtqc2JqNUNqNFN4Q3BpVSIgaGVpZ2h0PSIyMDAiIHdpZHRoPSIyMDAiLz4KPC9zdmc+',
         { method: 'GET' },
       ),
     )
     expect(result.status).toEqual(200)
     const text = await result.text()
-    expect(text).toEqual('request method: GET')
+    expect(text).toMatchInlineSnapshot(`
+      "<svg xmlns=\\"http://www.w3.org/2000/svg\\" width=\\"200\\" height=\\"200\\">
+          <image href=\\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMDAiIGhlaWdodD0iMjAwIj4KICAgIDxsaW5lIHgxPSIwIiB5MT0iMCIgeDI9IjIwMCIgeTI9IjIwMCIgc3Ryb2tlPSJkZWVwcGluayIgc3Ryb2tlLXdpZHRoPSI4Ii8+CiAgICA8bGluZSB4MT0iMCIgeTE9IjIwMCIgeDI9IjIwMCIgeTI9IjAiIHN0cm9rZT0iZGVlcHBpbmsiIHN0cm9rZS13aWR0aD0iOCIvPgo8L3N2Zz4=\\" height=\\"200\\" width=\\"200\\"/>
+      </svg>"
+    `)
   })
 })
 
